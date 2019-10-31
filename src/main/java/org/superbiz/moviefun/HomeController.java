9bcaba3f02ef9f633d3a5c085eafe1c1bfb0db6a
Martin Tobolak <martin.tobolak@esentri.com>
2019-10-31 09:28:08 +0000

    multiple database

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -1,6 +1,8 @@
 package org.superbiz.moviefun;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.superbiz.moviefun.albums.Album;
 import org.superbiz.moviefun.albums.AlbumFixtures;
@@ -19,12 +21,22 @@ public class HomeController {
     private final MovieFixtures movieFixtures;
     private final AlbumFixtures albumFixtures;
 
-    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+    private PlatformTransactionManager albumsTransactionManager;
+    private PlatformTransactionManager moviesTransactionManager;
+    private TransactionTemplate templateMovies;
+    private TransactionTemplate templateAlbums;
+
+    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures, PlatformTransactionManager getPlatformTransactionManagerAlbums, PlatformTransactionManager getPlatformTransactionManagerMovies) {
         this.moviesBean = moviesBean;
         this.albumsBean = albumsBean;
         this.movieFixtures = movieFixtures;
         this.albumFixtures = albumFixtures;
+        this.albumsTransactionManager = getPlatformTransactionManagerAlbums;
+        this.moviesTransactionManager = getPlatformTransactionManagerMovies;
+        this.templateMovies = new TransactionTemplate(moviesTransactionManager);
+        this.templateAlbums = new TransactionTemplate(albumsTransactionManager);
     }
+
 
     @GetMapping("/")
     public String index() {
@@ -33,13 +45,23 @@ public class HomeController {
 
     @GetMapping("/setup")
     public String setup(Map<String, Object> model) {
-        for (Movie movie : movieFixtures.load()) {
-            moviesBean.addMovie(movie);
-        }
+        templateMovies.execute(
+                (status) -> {
+                    for (Movie movie : movieFixtures.load()) {
+                        moviesBean.addMovie(movie);
+                    }
+                    return null;
+                }
+        );
 
-        for (Album album : albumFixtures.load()) {
-            albumsBean.addAlbum(album);
-        }
+        templateAlbums.execute(
+                (status) -> {
+                    for (Album album : albumFixtures.load()) {
+                        albumsBean.addAlbum(album);
+                    } return null;
+                }
+        );
+
 
         model.put("movies", moviesBean.getMovies());
         model.put("albums", albumsBean.getAlbums());
